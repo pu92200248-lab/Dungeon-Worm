1021aa8a6adc718e8d7baf09e674afef650fb124
pu92200248-lab <pu92200248@soyuo.mx>
2026-03-09 22:21:57 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "dungeon-worm-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./192.png",
+  "./512.png"
+];
+
+// Instalar y cachear archivos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Limpiar caches viejos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Servir desde cache, si no hay red
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => {
+        console.log("Sin conexión:", event.request.url);
+      });
+    })
+  );
+});
